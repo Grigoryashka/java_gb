8e8ef4798fb07ad86d43de547317f28f8375a1c1
Grigoryashka <vdjwhwjbw@gmail.com>
2023-07-04 21:51:11 +0500

    BinTree

---
+++ Mmain.java
@@ -0,0 +1,13 @@
+public class Mmain {
+    public static void main(String[] args) {
+        BinTree <Integer> tree = new BinTree<>();
+        tree.add(1);
+        tree.add(7);
+        tree.add(11);
+        tree.add(10);
+        tree.add(2);
+
+
+        tree.print();
+    }
+}
