87d16ca84c8e913dd5e094cacbfa08afa1ea66d2
Grigoryashka <vdjwhwjbw@gmail.com>
2023-07-14 19:45:55 +0500

    Calc with logging and last oper cancel

--- calc_logging.java
+++ calc_logging.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
@@ -10,45 +12,74 @@ public class calc_logging {
         } catch (IOException e) {
             System.err.println("Could not setup logger configuration: " + e.toString());
         }
-
+        
         calc();
     }
     private static void calc(){
-        System.out.println("\n");
-        System.out.println("Задача 3. Простой калькулятор. Введите первое число:");
+        Deque<String> queue = new LinkedList<>();
+        Double result = 0.0;
+
+        System.out.println("\nПростой калькулятор");
+
+        System.out.println("Введите число:");
         Double num1 = Double.parseDouble(System.console().readLine());
         Logging.write_log("First number is taken");
-        System.out.println("Введите второе число:");
-        Double num2 = Double.parseDouble(System.console().readLine());
-        Logging.write_log("Second number is taken");
-        
-        System.out.println("Введите операцию(умножить, разделить, сложить, вычесть, остаток от деления):");
-        String operation = System.console().readLine();
-        Logging.write_log("Operation is taken");
-        //System.out.println(operation);
-        Double result = 0.0;
-        switch(operation) {
-            case("умножить"):
-            result = num1 * num2;
-            break;
-            case("разделить"):
-            result = num1 / num2;
-            break;
-            case("сложить"):
-            result = num1 + num2;
-            break;
-            case("вычесть"):
-            result = num1 - num2;
-            break;
-            case("остаток от деления"):
-            result = num1 % num2;
-            break;
-            default:
-            break;
+        Double num_bet = null;
+        String operation = " ";
+
+        while (! operation.equals("null")){
+            
+
+            System.out.println("Введите число:");
+            Double num2 = Double.parseDouble(System.console().readLine());
+            Logging.write_log("Second number is taken");
+
+            System.out.println("Введите операцию(умножить, разделить, сложить, вычесть, остаток от деления, " +
+            "отмена(отмена последней операции)). Введите 'null', если хотите прервать программу:");
+            operation = System.console().readLine();
+            Logging.write_log("Operation is taken");
+            if (! operation.equals("отмена")) {
+                queue.add(operation);
+                Logging.write_log("Operation has been added to queue");
+            }
+
+            switch(operation) {
+                case("умножить"):
+                result = num1 * num2;
+                break;
+                case("разделить"):
+                result = num1 / num2;
+                break;
+                case("сложить"):
+                result = num1 + num2;
+                break;
+                case("вычесть"):
+                result = num1 - num2;
+                break;
+                case("остаток от деления"):
+                result = num1 % num2;
+                break;
+                case("отмена"):
+                if (queue.size() >= 1){
+                result = num_bet;
+                queue.removeLast();
+                break;
+                }
+                else {
+                    System.out.println("Вы должны совершить хотя бы одну операцию");
+                    break;
+                }
+                default:
+                break;
+            }
+            num_bet = num1;
+            num1 = result;
+            System.out.println("Результат: " + result);
+            Logging.write_log("Result is given");
+            System.out.println(queue);
         }
-        System.out.println(result);
-        Logging.write_log("Result is given");
-    }
+    }   
+
 }
 
 class Logging{
