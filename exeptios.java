a0596c65938b3ebc1a8df04ae54342836eaa8889
Grigoryashka <vdjwhwjbw@gmail.com>
2023-09-05 21:07:24 +0500

    Exeptions Java

--- exeptios.java
+++ exeptios.java
@@ -1,9 +1,29 @@
-﻿import java.util.Scanner;
+﻿
+import java.util.Scanner;
 
 public class exeptios {
     public static void main(String[] args) throws Exception{
-        System.out.println(number());
+        //System.out.println(number());
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 };
+            abc[3] = 9;
+        }catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        }
+    
+         
     }
+    public static void printSum(Integer a, Integer b){
+        System.out.println(a + b);
+    }
+
+    // TASKS 1 and 3
     public static Float number() throws Exception{
         Scanner in = new Scanner(System.in);
         Float num = null;
